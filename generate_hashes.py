9b036bef9813e2a67c64aa9b1e67b6bc439815db
Mustaphachafik <mustaphachafik.main@gmail.com>
2025-07-14 12:36:52 +0100

    Delete generate_hashes.py

--- generate_hashes.py
+++
@@ -1,5 +0,0 @@
-import hashlib
-
-print("jihane habati:", hashlib.sha256("Bluemoon@55".encode()).hexdigest())
-print("souhail soufiane:", hashlib.sha256("Redmoon@55".encode()).hexdigest())
-print("viewer:", hashlib.sha256("Moon@55".encode()).hexdigest())
